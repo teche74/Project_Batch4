9c14c26155fc3a3399ab35cc11934a09f810a3a7
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2026-04-04 22:00:52 +0530

    wait is private in Base Class , changed it to protected so that it will be accesible for childs

--- src/test/java/com/nopcommerce/promotions/base/BasePage.java
+++ src/test/java/com/nopcommerce/promotions/base/BasePage.java
@@ -11,7 +11,7 @@ import java.time.Duration;
 public class BasePage {
 
     protected WebDriver driver;
-    private WebDriverWait wait;
+    protected WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
