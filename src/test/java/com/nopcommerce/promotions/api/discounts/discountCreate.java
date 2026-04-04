8b8b35eead900e1d85f2ae093596a6711439647d
anushka.ladukar <maahiladukar@gmil.com>
2026-04-04 16:08:17 +0530

    updated

---
+++ src/test/java/com/nopcommerce/promotions/api/discounts/discountCreate.java
@@ -0,0 +1,38 @@
+package com.nopcommerce.promotions.api.discounts;
+
+import com.nopcommerce.promotions.api.base.Base;
+import io.restassured.response.Response;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.testng.annotations.Test;
+
+public class discountCreate extends Base {
+
+    @Test(dependsOnGroups = "login", groups = "createDiscounts")
+    public void testCreateDiscount() {
+        Response adminPage = request.given()
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)")
+                .get("/Admin/Discount/Create");
+
+        Document doc = Jsoup.parse(adminPage.getBody().asString());
+        String token = doc.select("input[name=__RequestVerificationToken]").attr("value");
+
+
+        request.given()
+                .cookies(cookies)
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("Name", "Anushka_Promo_API")
+                .formParam("DiscountTypeId", "1")
+                .formParam("UsePercentage", "true")
+                .formParam("DiscountPercentage", "15")
+                .formParam("IsActive", "true")
+                .formParam("__RequestVerificationToken", token)
+                .post("/Admin/Discount/Create")
+                .then()
+                .statusCode(302);
+
+
+        System.out.println("Discount Create Token: " + token);
+
+    }
+}
