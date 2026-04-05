bbee83a4c7cd34d96288181ec14ace2ce104deaf
shraddhajha-22 <125296910+shraddhajha-22@users.noreply.github.com>
2026-04-05 18:20:33 +0530

    Update AffiliatesPage.java

--- src/test/java/com/nopcommerce/promotions/pages/AffiliatesPage.java
+++ src/test/java/com/nopcommerce/promotions/pages/AffiliatesPage.java
@@ -1,43 +1,133 @@
 package com.nopcommerce.promotions.pages;
 
 import com.nopcommerce.promotions.base.BasePage;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+import java.util.List;
+
 
 public class AffiliatesPage extends BasePage {
 
     public AffiliatesPage(WebDriver driver) {
         super(driver);
     }
-
+    
     @FindBy(xpath = "//p[normalize-space()='Promotions']")
     WebElement promotionsMenu;
-
-    @FindBy(xpath = "//p[normalize-space()='Discounts']")
-    WebElement discountsSection;
 
     @FindBy(xpath = "//p[normalize-space()='Affiliates']")
     WebElement affiliatesSection;
 
-    @FindBy(xpath = "//p[normalize-space()='Newsletter subscribers']")
-    WebElement NewsletterSubscribersSection;
-
-    @FindBy(xpath = "//p[normalize-space()='Subscription types']")
-    WebElement SubscriptionTypesSection;
-
-    @FindBy(xpath = "//p[normalize-space()='Campaigns']")
-    WebElement CampaignsSection;
-
     @FindBy(xpath = "//h1[contains(text(),'Affiliates')]")
     WebElement pageTitle;
+    
+    @FindBy(xpath = "//a[@class='btn btn-primary']")
+    WebElement addNewBtn;
 
+    @FindBy(id = "Address_FirstName")
+    WebElement firstName;
+
+    @FindBy(id = "Address_LastName")
+    WebElement lastName;
+
+    @FindBy(id = "Address_Email")
+    WebElement email;
+
+    @FindBy(id = "Address_Company")
+    WebElement company;
+
+    @FindBy(id = "Address_CountryId")
+    WebElement countryDropdown;
+
+    @FindBy(id = "Address_StateProvinceId")
+    WebElement stateDropdown;
+
+    @FindBy(id = "Address_City")
+    WebElement city;
+
+    @FindBy(id = "Address_Address1")
+    WebElement address1;
+
+    @FindBy(xpath = "//button[@name ='save-continue']")
+    WebElement saveContinueBtn;
+
+    @FindBy(xpath = "//button[@name ='save']")
+    WebElement saveBtn;
+
+    @FindBy(xpath = "//table//tbody/tr")
+    List<WebElement> tableRows;
+
+    
+    @FindBy(xpath = "//table//a[contains(@href,'Edit')]")
+    WebElement editBtn;
+    
+    @FindBy(id = "affiliate-delete")
+    WebElement deleteBtn;
+
+    @FindBy(xpath = "//button[contains(@class,'btn-danger')]")
+    WebElement confirmDeleteBtn;
+
+    
     public void navigateToAffiliates() {
         click(promotionsMenu);
         click(affiliatesSection);
     }
-
+    
     public boolean isPageLoaded() {
         return isDisplayed(pageTitle);
+    }
+
+    public void addAffiliate(String fname, String lname, String mail) {
+
+        click(addNewBtn);
+        type(firstName, fname);
+        type(lastName, lname);
+        type(email, mail);
+        type(company, "Test Company");
+
+        Select country = new Select(countryDropdown);
+        country.selectByValue("237");
+
+        Select state = new Select(stateDropdown);
+        wait.until(d -> state.getOptions().size() > 1);
+        state.selectByVisibleText("New York");
+
+        type(city, "NY");
+        type(address1, "Street");
+
+        click(saveContinueBtn);
+        click(saveBtn);
+    }
+    
+    public int getAffiliateCount() {
+        return tableRows.size();
+    }
+
+
+    public void updateFirstAffiliate() {
+
+        for (int i = 0; i < 3; i++) {
+            try {
+                click(editBtn);
+                break;
+            } catch (StaleElementReferenceException e) {
+                System.out.println("Retry edit click...");
+            }
+        }
+
+        type(company, "UpdatedCompany");
+
+        click(saveBtn);
+    }
+
+
+    public void deleteFirstAffiliate() {
+
+        click(editBtn);
+        click(deleteBtn);
+        click(confirmDeleteBtn);
     }
 }
